1645237fd4382fc2c0978116171e253cd98f8d75
KietTram <tvkiet@tma.com.vn>
2016-06-27 15:32:40 +0700

    Commit chicken version 6

---
+++ chickenv6/NMSApp/src/main/java/object1/Main.java
@@ -0,0 +1,20 @@
+package object1;
+
+/*
+ * class to run
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		
+		// show frame and button START
+		Preparation preparation = new Preparation();
+		
+		// show grid, 5 anchors 
+		preparation.drawGridPanel();
+				
+		// click START button, read file and draw point
+		
+	}
+	
+}
