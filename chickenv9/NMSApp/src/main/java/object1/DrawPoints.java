d189442e7d58c31fd5070f0b95f39c3434f70453
KietTram <tvkiet@tma.com.vn>
2016-06-28 18:28:30 +0700

    Commit chicken version 9

---
+++ chickenv9/NMSApp/src/main/java/object1/DrawPoints.java
@@ -0,0 +1,49 @@
+package object1;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.geom.Ellipse2D;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JPanel;
+
+/*
+ * 1. Draw points by add those into a list and draw each one 
+ * ok class
+ */
+public class DrawPoints extends JPanel {
+	private static final long serialVersionUID = 1L;
+	
+	private List<Double> xList = new ArrayList<Double>();
+	private List<Double> yList= new ArrayList<Double>();
+	
+	public void addX(double x) {
+		xList.add(x);
+	}
+	
+	public void addY(double y) {
+		yList.add(y);
+	}
+	
+	@Override
+   public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		
+		Graphics2D g2d = (Graphics2D) g;
+				
+		for (int i = 0; i < xList.size(); i++) {
+			double x = xList.get(i);
+			double y = yList.get(i);
+			
+			// draw point here
+			Ellipse2D p = new Ellipse2D.Double(x, y ,10 ,10); // create a new ellipse with specific x and y coordinates
+			g2d.draw(p); // draw ellipse
+			
+			g2d.setColor(Color.RED); // set color 
+		    g2d.fill(p); // fill ellipse 
+		    
+		}		
+   }	
+}
---
+++ chickenv9/NMSApp/src/main/java/object1/Main.java
@@ -0,0 +1,22 @@
+package object1;
+
+/*
+ * class to run
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		
+		// show frame, grid, 5 anchors and button START
+		new Preparation();
+			
+		// click START button, read file and draw point
+		
+		
+		// do thiet ke hay sao ma cai frame no nho 1 ben vay?
+		// da, tu thiet ke
+		// sao ko cho grid full cai frame lun
+		// da, de em sua lai
+	}
+	
+}
